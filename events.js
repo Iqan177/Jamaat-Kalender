3d633997afa6ea5a3b62c8f290a2af5b1e961256
Iqan177 <khawajaiqan@gmail.com>
2025-11-02 22:01:31 +0100

    Add files via upload

---
+++ events.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/Event');
+
+// GET all events
+router.get('/events', async (req, res) => {
+    try {
+        const events = await Event.find().sort({ date: 1 });
+        res.json(events);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// POST new event
+router.post('/events', async (req, res) => {
+    try {
+        const event = new Event(req.body);
+        await event.save();
+        res.status(201).json(event);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// DELETE event
+router.delete('/events/:id', async (req, res) => {
+    try {
+        await Event.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Event deleted' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
+});
+
+
+module.exports = router;
